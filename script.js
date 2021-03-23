8c1db281394f01469059c940b59a992e3efd2d8f
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-23 16:52:02 +0000

    Assignment-4

--- script.js
+++ script.js
@@ -1,70 +1,70 @@
 document.getElementById('b1').addEventListener('click', change)
 
 function change () {
-  document.getElementById('paragraph').innerHTML = '<p>21. Disney<br><br>“Disney bought Pixar for $7.4 Billion in 2006. Jobs became a member of Disney’s board and the largest shareholder of the company” (Stieg).</p>'
+  document.getElementById('paragraph').innerHTML = '<p>1. Birth<br><br>At the age of 23, Steve Jobs learned the circumstances of his birth. His parents put him up for adoption, unnamed, with the stipulation that his adoptive parents must provide him with a college education. After the adoption, his biological parents married and had a daughter Mona Simpson, whom became an author (Biography.com).</p>'
   document.getElementById('right-img').src = './Images/1. Birth.PNG'
 }
 // --------------------------1
 document.getElementById('b2').addEventListener('click', change1)
 
 function change1 () {
-  document.getElementById('paragraph').innerHTML = '<p>22. Bill Gates and Steve Jobs Become Friends<br><br>2007 Gates appeared with Jobs on the “AllThingsD conference” (Hartmans, Weinberger), where the two actually expressed admiration for each other.</p>'
+  document.getElementById('paragraph').innerHTML = '<p>2. Childhood<br><br>In primary school, Steve’s teachers had to bribe him to get him to do his homework, however, he tested very well and his parents turned down their offer to send him to high school early. His father was a machinist and his mother was an accountant, and so it’s not a very big surprise that at the age of 12, Steve picked up the phone and called Mr. Hewlett from Hewlett-Packard to request the parts he needed to build a frequency counter, not only was he given the parts, but the following summer he worked at putting nuts and bolts together on frequency counters at Hewlett-Packard.</p>'
   document.getElementById('right-img').src = './Images/2. Childhood.PNG'
 }
 // --------------------------2
 document.getElementById('b3').addEventListener('click', change2)
 
 function change2 () {
-  document.getElementById('paragraph').innerHTML = '<p>23. iTunes<br><br>“In 2008, Apple became the second-biggest music retailer in America — second only to Walmart, fueled by iTunes and iPod sales”</p>'
+  document.getElementById('paragraph').innerHTML = '<p>3. High School<br><br>Steve met Steve Wozniak while he was in high school, though Wozniak was four years his senior, they shared an interest in computers as well as pranks. Wozniak created a “Blue Box” which tricked phones into allowing the users to make free calls anywhere in the world, and Jobs found ways to sell them (Lapsley).</p>'
   document.getElementById('right-img').src = './Images/3. High School.PNG'
 }
 // --------------------------3
 document.getElementById('b4').addEventListener('click', change3)
 
 function change3 () {
-  document.getElementById('paragraph').innerHTML = '<p>24. Movie I<br><br>In 2013, the movie Jobs was released, starring Ashton Kutcher.</p>'
+  document.getElementById('paragraph').innerHTML = '<p>4. College<br><br>After graduating from high school in 1972, Jobs attended college for one semester, he attended classes that were being offered at the college out of interest despite not getting credits (CNET News Staff). He developed a love of Calligraphy which manifested itself into a love of fonts during his time at Apple Computers (Baker). He became somewhat of a hippy during this time, partaking in psychedelic drugs, eastern philosophy and a diet of only fruit (fruitarian), his short time working on an orchard, which Wozniak says was actually a commune, might have been his inspiration for the Companies name (Baker). </p>'
   document.getElementById('right-img').src = './Images/4. College.PNG'
 }
 // --------------------------4
 document.getElementById('b5').addEventListener('click', change4)
 
 function change4 () {
-  document.getElementById('paragraph').innerHTML = '<p>25. Movie II<br><br>In 2015, the movie “Steve Jobs” was released, it won the Golden Globe Award for Best Screenplay.</p>'
+  document.getElementById('paragraph').innerHTML = '<p>5. Lisa<br><br>This is when he met the girlfriend who would be the mother of his child, Lisa, although Jobs would deny being the child’s father until she was 7 years old, she lived with him as a teenager (Biography.com).</p>'
   document.getElementById('right-img').src = './Images/5. Lisa.PNG'
 }
 // --------------------------5
 document.getElementById('b6').addEventListener('click', change5)
 
 function change5 () {
-  document.getElementById('paragraph').innerHTML = '<p>26. Success<br><br>“In 2018, Apple officially hit the $1 trillion mark, making it the first American Company to do so, ever” (Weinberger, Hartmans). He gave himself a $1 annual salary (Biography.com)(Shwarts).</p>'
+  document.getElementById('paragraph').innerHTML = '<p>6. Atari<br><br>Wozniak was hired to work as a technician at Atari, for the man who created the game Pong, Al Acorn. Though he had great ideas for the games, he did not get along well with his colleagues, whom complained not just about his attitude, but about his hygiene too. Acorn solved these problems by giving Jobs the only night shift. When the company created the new game ‘Breakout’, they offered up to a $5k bonus for anyone who could reduce the number of expensive TTL chips needed to run the game. Wozniak worked on solving this engineering problem while working at Hewlett-Packard during the day, and sneaking in Atari to work with Jobs at night. Jobs told Wozniak that he would give him half of a $700 bonus. During his time at Atari, he took a backpacking trip around India, it is said that he partook in a great deal of psychedelic drugs during this trip.	</p>'
   document.getElementById('right-img').src = './Images/6. Atari.PNG'
 }
 // --------------------------6
 document.getElementById('b7').addEventListener('click', change6)
 
 function change6 () {
-  document.getElementById('paragraph').innerHTML = '<p>27. iPhone<br><br>Apple introduced the first iPhone in 2007, and “owns 19.1 percent of the smartphone market as of August 2011” (CNET News Staff). <br><br>Jobs was “[i]nducted in the California Hall of Fame by Gov. Arnold Schwartzenegger” (CNET News Staff”.</p>'
+  document.getElementById('paragraph').innerHTML = '<p>7. Apple<br><br>Jobs and Wozniak “were also active in the Homebrew Computer Club, a group of computer hobbyists, where Wozniak`s homemade computer drew attention from other enthusiasts” (Metz). In 1976 Steve Jobs co-founded Apple Computer from Jobs’ adoptive parents’ garage with Wozniak and Ronald Wayne. Wayne left soon after the company was founded (Baker). They sold the Apple I in the form of a kit for $666.66 (CNET News Staff). It didn’t come with a mouse, a keyboard or even a case, they simply had the idea to make “computers small enough for people to have in their homes or offices” (Richardson). In 1977, Acorn introduced Jobs to “venture capitalists” (Cifaldi). and Jobs Incorporated Apple. They added a case to the computer and the first ever colour graphics (Richardson) creating the Apple II. On “December 12th, 1980 Apple goes public, putting Jobs’ net worth north of $200 million (CNET News Staff). </p>'
   document.getElementById('right-img').src = './Images/7. Apple.PNG'
 }
 // --------------------------7
 document.getElementById('b8').addEventListener('click', change7)
 
 function change7 () {
-  document.getElementById('paragraph').innerHTML = '<p>28.Illness and Death<br><br>“January 17th 2011: Goes on medical leave of absence from CEO nearly two years after the six month break he took to undergo a liver transplant”, still he “makes a surprise appearance to take the wraps off the iPad 2” in March 2011. His last appearance at an Apple event took place on June 6th, 2011, and he pitched the “Cupertino on a new Apple campus.<br><br>August 9th, 2011, Apple shares edge past those of Exxon Mobil to become the most valuable U.S. company, in terms of market capitalization, at $337.17 billion. August 24th, 2011 [Jobs] resigns from Apple CEO post; becomes chairman (CNET News Staff). On Octer 5th, 2011: Apple Announces that Steve Jobs has passed away (Cammeroon).</p>'
+  document.getElementById('paragraph').innerHTML = '<p>8. Bill Gates<br><br>“Microsoft made software early on for the mega-popular Apple II PC, and [Bill] Gates would routinely fly down […] to see what Apple was working on” (Hartmans, Weinberger), but Gates “wasn’t particularly impressed with what he saw as a limited platform [for the new Apple Macintosh], or Jobs’ attitude. […] Still Gates appeared alongside Jobs in a 1983 video – a “dating Game” riff – screened for Apple employees ahead of the Macintosh’s launch” (Hartmans, Weinberger).</p>'
   document.getElementById('right-img').src = './Images/8. Bill Gates.PNG'
 }
 // --------------------------8
 document.getElementById('b9').addEventListener('click', change8)
 
 function change8 () {
-  document.getElementById('paragraph').innerHTML = '<p>29.Grief<br><br>“His death was followed by an outpouring of grief around the world from Apple fans and competitors, as well as heads of state.<br><br>In a sign of how pervasive the gadgets he spearheaded have become, much of the mourning was done on Apple gadgets: People held up pictures of candles on their iPads, reviewed his life on Macintosh Computers and tapped out tributes on iPhones” (Metz).</p>'
+  document.getElementById('paragraph').innerHTML = '<p>9. Jobs Quit Apple<br><br>Wozniak lost interest in the day-to-day running of Apple and left the company in 1983. Jobs then hired ”PepsiCo`s John Sculley to be president” (Richardson). In an effort to compete with IBM, Jobs spent a great deal on a famous “1984” commercial that played at the Superbowl, the board members, including Sculley, didn’t like how much Jobs was spending on the making of or the advertising of the Macintosh. “In 1984, Apple released the Macintosh, marketing the computer as a piece of a counterculture lifestyle: romantic, youthful, creative. But despite positive sales and performance superior to IBM`s PCs, the Macintosh was still not IBM-compatible. In fact, “a furious Jobs accused Gates and Microsoft of ripping off the Macintosh [due to the company announcing it’s first version of Windows], but Gates didn’t care – he knew that graphical interfaces would be big, and didn’t think Apple had the exclusive rights to the idea […], [b]esides, […] Apple took the idea […] from the Xerox PARC labs” (Hartman, Weinberger). Sculley Believed Jobs was hurting Apple” (Biography.com). Jobs quit from Apple on September 16th, 1985 after losing a boardroom battle for control of the company from Sculley (Kahney).</p>'
   document.getElementById('right-img').src = './Images/9. Jobs Quit Apple.PNG'
 }
 // --------------------------9
 document.getElementById('b10').addEventListener('click', change9)
 
 function change9 () {
-  document.getElementById('paragraph').innerHTML = '<p>30. references<br><br></p>'
+  document.getElementById('paragraph').innerHTML = '<p>10. Bill Gates Quote<br><br>“When Jobs accused Gates of stealing the idea, he famously answered: "Well, Steve, I think there`s more than one way of looking at it. I think it`s more like we both had this rich neighbor named Xerox and I broke into his house to steal the TV set and found out that you had already stolen it“” (Hartman, Weinberger).</p>'
   document.getElementById('right-img').src = './Images/10. Bill Gates Quote.PNG'
 }
 // --------------------------10
